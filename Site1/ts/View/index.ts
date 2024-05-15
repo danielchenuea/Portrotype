e585df5bd4ad9f7c20f9f3599101d91108a292b6
DANIEL CHEN <dchen@avipam.com.br>
2024-05-15 17:52:45 -0300

    commit inicial

---
+++ Site1/ts/View/index.ts
@@ -0,0 +1,9 @@
+
+$(function() {
+    $("#index_Header").load("../MainPageHeader.html");  
+    let includes = $('[data-include]')
+    $.each(includes, function () {
+      let file = 'views/MainPageView/' + $(this).data('include') + '.html'
+      $(this).load(file)
+    })
+});
